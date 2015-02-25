ba9681d6477be1fd198b8491d79a929d872c3525
Jonathan Duss <jonathan.duss@epfl.ch>
2015-02-25 11:45:14 +0100

    remove code that get informations about apps from PeriodicTaskReceiver. Is now in ApplicationsInformation

--- Appspy/app/src/main/java/com/epfl/appspy/ApplicationsInformation.java
+++ Appspy/app/src/main/java/com/epfl/appspy/ApplicationsInformation.java
@@ -1,7 +1,181 @@
 package com.epfl.appspy;
 
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
 /**
+ * Class providing functions to get information about the app installed on the device.
+ *
  * Created by Jonathan Duss on 25.02.15.
  */
 public class ApplicationsInformation {
+
+    private Context context;
+    private PackageManager packageManager = null;
+
+    public ApplicationsInformation(Context context){
+        this.context = context;
+        packageManager = context.getPackageManager();
+    }
+
+    /**
+     * Returns the installed app
+     *
+     * @return List of non-system apps
+     */
+    public List<PackageInfo> getInstalledApps(boolean includeSystem) {
+
+        List<PackageInfo> allPkg = packageManager.getInstalledPackages(PackageManager.GET_PERMISSIONS);
+
+        List<PackageInfo> installedApps = new ArrayList<>();
+
+        for (PackageInfo pkg : allPkg) {
+
+            //we add the app to the list if it is not system OR if we include system apps
+            if ((isSystem(pkg) == false) || includeSystem) {
+                installedApps.add(pkg);
+            }
+        }
+
+        return installedApps;
+
+
+
+        //Try get currently used app
+
+
+    }
+
+    /**
+     * Returns the associated permissions of the apps given in argument
+     *
+     * @param apps
+     * @return
+     */
+    public Hashtable<PackageInfo, String[]> getAppsPermissions(List<PackageInfo> apps) {
+
+        Hashtable<PackageInfo, String[]> permissions = new Hashtable<>();
+
+        for (PackageInfo pkg : apps) {
+
+            String[] appPermissions = pkg.requestedPermissions;
+
+            if (appPermissions == null) {
+                String[] noPermission = {"No permissions"};
+                appPermissions = noPermission;
+            }
+
+            permissions.put(pkg, appPermissions);
+        }
+
+        return permissions;
+    }
+
+    /**
+     * Return all the active applications, in background or foreground
+     *
+     * @param includeSystem set to true to include also system apps
+     * @return list of active apps
+     */
+    public List<PackageInfo> getActiveApps(boolean includeSystem) {
+
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> tasks = activityManager.getRunningAppProcesses();
+        List<PackageInfo> activeApps = new ArrayList<>();
+
+
+        for (ActivityManager.RunningAppProcessInfo task : tasks) {
+
+            String[] pkgsString = task.pkgList;
+
+            for (String pkgString : pkgsString) {
+
+                try {
+                    PackageInfo packageInfo = packageManager.getPackageInfo(pkgString, PackageManager.GET_META_DATA);
+
+                    //we add the app to the list if it is not system OR if we include system apps
+                    if ((isSystem(packageInfo) == false) || includeSystem) {
+                        activeApps.add(packageInfo);
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    //nothing to do
+                }
+            }
+        }
+        return activeApps;
+    }
+
+
+    /**
+     * Return the app that is currently displayed on the screen, used by the user
+     * @return The app the user is using now or null otherwise
+     */
+    public PackageInfo getCurrentlyUsedApp(boolean includeSystem) {
+
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> tasks = activityManager.getRunningAppProcesses();
+        List<PackageInfo> activeApps = new ArrayList<>();
+
+        for (ActivityManager.RunningAppProcessInfo task : tasks) {
+
+            //Check if the task is the one on foreground, thus, displayed on the screen and currently used by the user
+            if(task.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                String[] pkgsString = task.pkgList;
+
+                for(String pkgName : pkgsString) {
+
+                    try {
+                        PackageInfo packageInfo = packageManager.getPackageInfo(pkgName, PackageManager.GET_META_DATA);
+
+                        if ((isSystem(packageInfo) == false) || includeSystem) {
+                            //exclude our app from it (because always on foreground) ???
+                            if(context.getApplicationInfo().processName.equals(packageInfo.applicationInfo.processName) == false) {
+                                Log.d("Appspy", "" + getAppName(packageInfo));
+                                return packageInfo;
+                            }
+                        }
+
+                    } catch (PackageManager.NameNotFoundException e) {
+                        //nothing to do
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Return if a given package belogs to the system
+     * @param pi the package
+     * @return if the package is part of the system
+     */
+    public boolean isSystem(PackageInfo pi) {
+        // check if bit the for the flag "system" is 1.
+        // if is NOT system, (f1 & flag_system) = 0
+        //is system if (f1 & flag_system) != 0
+        // (basic bitwise operation)
+
+        return (pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+    }
+
+
+    /**
+     * Return the name of the application to which this package belogs to
+     * @param packageInfo the packageInfo
+     * @return name of the app
+     */
+    public String getAppName(PackageInfo packageInfo){
+        return (String) packageInfo.applicationInfo.loadLabel(packageManager);
+    }
+
 }
--- Appspy/app/src/main/java/com/epfl/appspy/PeriodicTaskReceiver.java
+++ Appspy/app/src/main/java/com/epfl/appspy/PeriodicTaskReceiver.java
@@ -12,6 +12,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Dictionary;
@@ -19,36 +20,75 @@ import java.util.Hashtable;
 import java.util.List;
 
 
+/**
+ * Check
+ */
 public class PeriodicTaskReceiver extends BroadcastReceiver {
     private static final String TAG = "PeriodicTaskReceiver";
 
     private static final String EXTRA = "extra";
-    private enum EXTRA_ACTION_PERIODICITY {HALF_HOUR, MINUTE, TEN_SECONDS };
+    private enum EXTRA_ACTION_PERIODICITY {NONE, HALF_HOUR, MINUTE, TEN_SECONDS };
     private final int NO_EXTRA = -1;
+
+    private static Context context;
+
+    private static ApplicationsInformation appInformation;
+
+
 
 
     public void createAlarms(Context context, Intent intent){
         Log.d("Appspy", "Alarm is set");
-        Intent backgroundChecker = new Intent(context, PeriodicTaskReceiver.class);
-        backgroundChecker.setAction(Intent.ACTION_SEND);
-        backgroundChecker.putExtra(EXTRA, EXTRA_ACTION_PERIODICITY.TEN_SECONDS);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, backgroundChecker, 0);
-
 
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        int interval = 10000;
 
-        manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+
+        final int tenSeconds = 10000;
+        final int minute = 60000;
+        final int halfHour = 30000; //60000 * 30; //For now: 30 seconds
+        final int CODE_ONE = 12323;
+        final int CODE_TWO = 12324;
+
+        Intent backgroundChecker;
+        PendingIntent pendingIntent;
+
+
+        //Ten second periodicity
+        backgroundChecker = new Intent(context, PeriodicTaskReceiver.class);
+        backgroundChecker.setAction(Intent.ACTION_SEND);
+        backgroundChecker.putExtra(EXTRA, EXTRA_ACTION_PERIODICITY.TEN_SECONDS);
+        pendingIntent = PendingIntent.getBroadcast(context, CODE_ONE, backgroundChecker, PendingIntent.FLAG_CANCEL_CURRENT);
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), tenSeconds, pendingIntent);
+
+        //Halft hour periodicity
+        backgroundChecker = null;
+        pendingIntent = null;
+        backgroundChecker = new Intent(context, PeriodicTaskReceiver.class);
+        backgroundChecker.setAction(Intent.ACTION_SEND);
+        backgroundChecker.putExtra(EXTRA, EXTRA_ACTION_PERIODICITY.HALF_HOUR);
+        pendingIntent = PendingIntent.getBroadcast(context, CODE_TWO, backgroundChecker, PendingIntent.FLAG_CANCEL_CURRENT);
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), halfHour, pendingIntent);
 
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Log.d("Appspy","INTENT IS ROCO: " + intent);
+        // Log.d("Appspy","INTENT IS ROCO: " + intent);
         Log.d("Appspy","ACTION: " + intent.getAction());
 
+        //Init class members
+        if(this.context == null || this.appInformation == null){
+            this.context = context;
+            appInformation = new ApplicationsInformation(context);
+        }
+
+
+        //Process the broadcast message
         if(intent.getAction() != null) {
+
+            //Log.d("Appspy", "HEY: " + intent.getExtras().containsKey(EXTRA));
+
 
             if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
                 // Register your reporting alarms here.
@@ -56,29 +96,17 @@ public class PeriodicTaskReceiver extends BroadcastReceiver {
 
             } else if (intent.getAction().equals(Intent.ACTION_SEND)
                     && (EXTRA_ACTION_PERIODICITY) intent.getSerializableExtra(EXTRA) == EXTRA_ACTION_PERIODICITY.TEN_SECONDS ){
+
+                periodicCheckTenSeconds();
+
                 Log.d("Appspy", "%%%%%%%%%%%% PERIODIC TASK every 10 seconds");
 
-                getCurrentlyUsedApp(context, false);
+                appInformation.getCurrentlyUsedApp(false);
 
-                List<PackageInfo> installedApps = getInstalledApps(context, false);
-                Hashtable<PackageInfo, String[]> permissionsForApp = getAppsPermissions(installedApps, context);
-                List<PackageInfo> activeApps = getActiveApps(context, false);
+                List<PackageInfo> activeApps = appInformation.getActiveApps(false);
 
                 //log
 
-                Log.d("Appspy", "-------------------------------");
-                Log.d("Appspy", "Permissions");
-                Log.d("Appspy", "-------------------------------");
-                for (PackageInfo app : installedApps) {
-                    String[] permissions = permissionsForApp.get(app);
-                    Log.d("Appspy", "" + app.applicationInfo.loadLabel(context.getPackageManager()));
-
-                    for (String p : permissions) {
-                        Log.d("Appspy", p);
-                    }
-                    Log.d("Appspy", "===============================");
-
-                }
 
 
                 Log.d("Appspy", "-------------------------------");
@@ -87,188 +115,47 @@ public class PeriodicTaskReceiver extends BroadcastReceiver {
                 for (PackageInfo app : activeApps) {
                     Log.d("Appspy", "" + app.applicationInfo.loadLabel(context.getPackageManager()));
                 }
-            }
-        }
-    }
+            } else if (intent.getAction().equals(Intent.ACTION_SEND)
+                    && (EXTRA_ACTION_PERIODICITY) intent.getSerializableExtra(EXTRA) == EXTRA_ACTION_PERIODICITY.HALF_HOUR ){
+                Log.d("Appspy", "%%%%%%%%%%%% PERIODIC TASK every 30 minutes");
 
+                List<PackageInfo> installedApps = appInformation.getInstalledApps(false);
+                Hashtable<PackageInfo, String[]> permissionsForApp = appInformation.getAppsPermissions(installedApps);
 
-/*    private void doPeriodicTask(Context context, Application myApplication) {
-        // Periodic task(s) go here ...
-        Log.d("Appspy", "%%%%%%%%%%%%%% DO PER TASK");
-    }
+                Log.d("Appspy", "-------------------------------");
+                Log.d("Appspy", "Permissions");
+                Log.d("Appspy", "-------------------------------");
 
+                for (PackageInfo app : installedApps) {
+                    String[] permissions = permissionsForApp.get(app);
+                    Log.d("Appspy", "" + appInformation.getAppName(app));
 
-    public void restartPeriodicTaskHeartBeat(Context context, Application myApplication) {
-
-        Log.d("Appspy", "RESTART PER TASK");
-    }
-
-
-    public void stopPeriodicTaskHeartBeat(Context context) {
-        Log.d("Appspy", "STOP PER TASK");
-
-    }*/
-
-
-    /**
-     * Returns the installed app
-     *
-     * @param context
-     * @return List of non-system apps
-     */
-    public List<PackageInfo> getInstalledApps(Context context, boolean includeSystem) {
-        PackageManager packageManager = context.getPackageManager();
-
-        List<PackageInfo> allPkg = packageManager.getInstalledPackages(PackageManager.GET_PERMISSIONS);
-
-        List<PackageInfo> installedApps = new ArrayList<>();
-
-        for (PackageInfo pkg : allPkg) {
-
-            //we add the app to the list if it is not system OR if we include system apps
-            if ((isSystem(pkg) == false) || includeSystem) {
-                installedApps.add(pkg);
-            }
-        }
-
-        return installedApps;
-
-
-
-        //Try get currently used app
-
-
-    }
-
-
-    /**
-     * Returns the associated permissions of the apps given in argument
-     *
-     * @param apps
-     * @param context
-     * @return
-     */
-    public Hashtable<PackageInfo, String[]> getAppsPermissions(List<PackageInfo> apps, Context context) {
-        PackageManager packageManager = context.getPackageManager();
-
-        Log.d("Appspy", "-------------------------------------------------------");
-        Log.d("Appspy", "-------------------------------------------------------");
-        Log.d("Appspy", "LOGGING PERMISSION OF ALL THIRD PARTY APPLICATION");
-
-        Hashtable<PackageInfo, String[]> permissions = new Hashtable<>();
-
-        for (PackageInfo pkg : apps) {
-
-            Log.d("Appspy", "##########");
-            Log.d("Appspy", "-" + pkg.applicationInfo.loadLabel(packageManager));
-
-            String[] appPermissions = pkg.requestedPermissions;
-
-            if (appPermissions == null) {
-                String[] noPermission = {"No permissions"};
-                appPermissions = noPermission;
-            }
-
-            permissions.put(pkg, appPermissions);
-
-            //output log
-            for (String permission : appPermissions) {
-                Log.d("Appspy", permission);
-            }
-
-
-        }
-
-        return permissions;
-    }
-
-
-    /**
-     * Return all the active applications
-     *
-     * @param context
-     * @param includeSystem set to true to include also system apps
-     * @return list of active apps
-     */
-    public List<PackageInfo> getActiveApps(Context context, boolean includeSystem) {
-
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> tasks = activityManager.getRunningAppProcesses();
-        List<PackageInfo> activeApps = new ArrayList<>();
-
-        for (ActivityManager.RunningAppProcessInfo task : tasks) {
-
-            String[] pkgsString = task.pkgList;
-
-            for (String pkgString : pkgsString) {
-
-
-                PackageManager pm = context.getPackageManager();
-
-                try {
-                    PackageInfo packageInfo = pm.getPackageInfo(pkgString, PackageManager.GET_META_DATA);
-
-                    //we add the app to the list if it is not system OR if we include system apps
-                    if ((isSystem(packageInfo) == false) || includeSystem) {
-                        activeApps.add(packageInfo);
+                    for (String p : permissions) {
+                        Log.d("Appspy", p);
                     }
-                } catch (PackageManager.NameNotFoundException e) {
-                    //nothing to do
+                    Log.d("Appspy", "===============================");
+
                 }
+
             }
         }
-        return activeApps;
     }
 
 
-    /**
-     * Return the app that is currently explicitely (displayed on the screen) used by the user
-     * @param context
-     * @return The app the user is using now or null otherwise
-     */
-    public PackageInfo getCurrentlyUsedApp(Context context, boolean includeSystem) {
-
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> tasks = activityManager.getRunningAppProcesses();
-        List<PackageInfo> activeApps = new ArrayList<>();
-
-        for (ActivityManager.RunningAppProcessInfo task : tasks) {
-
-            //Check if the task is the one on foreground, thus, displayed on the screen and currently used by the user
-            if(task.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
-                PackageManager pm = context.getPackageManager();
-                String[] pkgsString = task.pkgList;
-
-                for(String pkgName : pkgsString) {
-
-                    try {
-                        PackageInfo packageInfo = pm.getPackageInfo(pkgName, PackageManager.GET_META_DATA);
-
-                        if ((isSystem(packageInfo) == false) || includeSystem) {
-                            //exclude our app from it (because always on foreground) ???
-                            if(context.getApplicationInfo().processName.equals(packageInfo.applicationInfo.processName) == false) {
-                                Log.d("Appspy", "" + packageInfo.applicationInfo.loadLabel(pm));
-                                return packageInfo;
-                            }
-                        }
-
-                    } catch (PackageManager.NameNotFoundException e) {
-                        //nothing to do
-                    }
-                }
-            }
-        }
-        return null;
-    }
+public void periodicCheckTenSeconds(){
+    Log.d("Appspy","Context: " + context);
+}
 
 
-    public boolean isSystem(PackageInfo pi) {
-        // check if bit the for the flag "system" is 1.
-        // if is NOT system, (f1 & flag_system) = 0
-        //is system if (f1 & flag_system) != 0
-        // (basic bitwise operation)
 
-        return (pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
-    }
+
+
+
+
+
+
+
+
+
 
 }
