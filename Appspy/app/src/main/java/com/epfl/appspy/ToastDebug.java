92d62d62663d463569c28c06481db496e7ff2397
Jonathan Duss <jonathan.duss@epfl.ch>
2015-04-10 18:50:33 +0200

    add toastDebug class

---
+++ Appspy/app/src/main/java/com/epfl/appspy/ToastDebug.java
@@ -0,0 +1,29 @@
+package com.epfl.appspy;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Jonathan Duss on 10.04.15.
+ */
+public class ToastDebug {
+
+    Toast t;
+
+    private ToastDebug(Toast t){
+        this.t = t;
+    }
+
+    public static ToastDebug makeText(Context context, String message, int duration){
+        return new ToastDebug(Toast.makeText(context, message, duration));
+    }
+
+    public void show(){
+        if(GlobalConstant.DEBUG) {
+            t.show();
+        }
+    }
+
+
+
+}
