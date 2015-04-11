301cf7e129eb28dd7c16a1367d0313212cc46a89
Jonathan Duss <jonathan.duss@epfl.ch>
2015-04-11 22:48:48 +0200

    - add close for CPUInfo

---
+++ Appspy/app/src/main/java/com/epfl/appspy/com/epfl/appspy/monitoring/CPUInfo.java
@@ -0,0 +1,23 @@
+package com.epfl.appspy.com.epfl.appspy.monitoring;
+
+/**
+ * Created by Jonathan Duss on 11.04.15.
+ */
+public class CPUInfo {
+
+    private int pid;
+    public double averageCpuUsage;
+    public int maxCpuUsage;
+
+
+    public CPUInfo(int pid) {
+        this.pid = pid;
+        averageCpuUsage = 0;
+        maxCpuUsage = 0;
+    }
+
+
+    public int getPid() {
+        return pid;
+    }
+}
