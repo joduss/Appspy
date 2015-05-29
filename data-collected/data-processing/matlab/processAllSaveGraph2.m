39d1366087ec1850b7cbff4a8a07b9696740f577
Jonathan Duss <jonathan.duss@epfl.ch>
2015-05-29 09:15:45 +0200

    - slight speedup - fix potentiel missing plot

--- data-collected/data-processing/matlab/processAllSaveGraph2.m
+++ data-collected/data-processing/matlab/processAllSaveGraph2.m
@@ -13,7 +13,7 @@ logYaxis = 1; %display log scale? (1 = true)
 axisLink = 'xy'; %x, y or xy
 showParam = 'a'; %a = all, b = back, f = fore
 offset=3;
-aggregatedTime = 6*60; %1 = no aggregation
+aggregatedTime = 2*60; %1 = no aggregation
 
 %TO ADAPT FIGURE SIZE: LOOK FOR "f = figure"
 
@@ -33,9 +33,9 @@ for nameIdx = 1 : numel(dbnames)
     packagesNames = allPackageName(database);
     
     for packageName = packagesNames
-        packageName = packageName{1};               
-                
-        results = sqlite3.execute(database, strcat('SELECT * from table_applications_activity WHERE package_name =''', packageName, ''' AND uploaded_data>0 AND record_time > (SELECT record_time from table_applications_activity where record_id=1 limit 1) ORDER BY record_time'));
+        packageName = packageName{1};
+        
+        results = sqlite3.execute(database, strcat('SELECT * from table_applications_activity WHERE package_name =''', packageName, ''' AND record_time > (SELECT record_time from table_applications_activity where record_id=1 limit 1) ORDER BY record_time'));
         
         dataX_back = zeros(numel(results),1);
         dataY_back = zeros(numel(results),1);
@@ -65,8 +65,12 @@ for nameIdx = 1 : numel(dbnames)
             end
         end
         
+        %         %PREPROCESSING (done one the data are put in array dataY_...
+        %         %up/down value < 0 set to 0
+        %         idx = find([results.uploaded_data] < 0); a = mat2cell(zeros(1,numel(idx))+5, 1, ones(1,numel(idx))) ;[results(idx).uploaded_data] = a{:};
         
-        %PROCESS 
+        
+        %PROCESS
         for rowIdx = 1:numel(results)
             timestamp = (results(rowIdx).record_time+2*60*60000)/1000;
             time = [1970 1 1 0 0 timestamp];
@@ -107,71 +111,72 @@ for nameIdx = 1 : numel(dbnames)
             lastRow{mod(rowIdx,offset)+1} = row;
         end
         
-        %clean: remove where record_time = 0
-        idxZero = find(dataX_back ~= 0 & dataY_back ~= 0);
+        %clean: remove where record_time = 0 and where data > 0
+        % (keep only the others)
+        idxZero = find(dataX_back > 0 & dataY_back > 0);
         dataX_back = dataX_back(idxZero);
         dataY_back = dataY_back(idxZero);
         
-        idxZero = find(dataX_fore ~= 0 & dataY_fore ~= 0);
+        idxZero = find(dataX_fore > 0 & dataY_fore > 0);
         dataX_fore = dataX_fore(idxZero);
         dataY_fore = dataY_fore(idxZero);
         
-        idxZero = find(dataX_ibtw ~= 0 & dataY_ibtw ~= 0);
+        idxZero = find(dataX_ibtw > 0 & dataY_ibtw > 0);
         dataX_ibtw = dataX_ibtw(idxZero);
         dataY_ibtw = dataY_ibtw(idxZero);
         
         %clean the same way, but for down
-        idxZero = find(dataX_down_back ~= 0 & dataY_down_back ~= 0);
+        idxZero = find(dataX_down_back > 0 & dataY_down_back > 0);
         dataX_down_back = dataX_down_back(idxZero);
         dataY_down_back = dataY_down_back(idxZero);
         
-        idxZero = find(dataX_down_fore ~= 0 & dataY_down_fore ~= 0);
+        idxZero = find(dataX_down_fore > 0 & dataY_down_fore > 0);
         dataX_down_fore = dataX_down_fore(idxZero);
         dataY_down_fore = dataY_down_fore(idxZero);
         
-        idxZero = find(dataX_down_ibtw ~= 0 & dataY_down_ibtw ~= 0);
+        idxZero = find(dataX_down_ibtw > 0 & dataY_down_ibtw > 0);
         dataX_down_ibtw = dataX_down_ibtw(idxZero);
         dataY_down_ibtw = dataY_down_ibtw(idxZero);
         
         %aggregate
+        
         if(aggregatedTime > 1)
             [ dataX_back, dataY_back, dataX_fore, dataY_fore, dataX_ibtw, dataY_ibtw ] = aggregateData(dataX_back, dataY_back, dataX_fore, dataY_fore, dataX_ibtw, dataY_ibtw, aggregatedTime);
             [ dataX_down_back, dataY_down_back, dataX_down_fore, dataY_down_fore, dataX_down_ibtw, dataY_down_ibtw ] = aggregateData(dataX_down_back, dataY_down_back, dataX_down_fore, dataY_down_fore, dataX_down_ibtw, dataY_down_ibtw, aggregatedTime);
         end
-        
         %% NOW PLOT THE DATA
         
         %find y axis limit, same for all graph to be able to compare
         [minY_upload, maxY_upload, minY_download, maxY_download, minY_global, maxY_global] = findCommonAxisLimits(databases, packageName);
         
         
-        
         if(strcmp(type, 'bar'))
             %% PLOT BAR
             titleFontSize = 28;
             axisLabelFontSize = 24;
-            figureNameUpload = strcat('figures/',dbName,'-',packageName,'-aggr',num2str(aggregatedTime), '-bar-upload.pdf');
-            figureNameDownload = strcat('figures/',dbName,'-',packageName,'-aggr',num2str(aggregatedTime), '-bar-download.pdf');
             
-            close all;           
-            fig_upload = plotBar(dataX_back, dataY_back, dataX_fore, dataY_fore, dataX_ibtw, dataY_ibtw, showParam, logYaxis, 'off');
-            title(strcat({'Uploaded data for '},dbName,'-',packageName, {'  aggr. '}, num2str(aggregatedTime), {'min.'}),'FontSize',titleFontSize);
-            ylabel('Uploaded data [kB]','FontSize',axisLabelFontSize);
-            set(gca, 'FontSize', axisLabelFontSize);
-            grid on;
-            ylim([minY_global, maxY_global]);
+            close all;
             if(numel([dataX_back(:)', dataX_fore(:)', dataX_ibtw(:)']) > 0)
+                
+                figureNameUpload = strcat('figures/',packageName,'-',dbName,'-aggr',num2str(aggregatedTime), '-bar-upload.pdf');
+                figureNameDownload = strcat('figures/',packageName,'-',dbName,'-aggr',num2str(aggregatedTime), '-bar-download.pdf');
+                fig_upload = plotBar(dataX_back, dataY_back, dataX_fore, dataY_fore, dataX_ibtw, dataY_ibtw, showParam, logYaxis, 'off');
+                title(strcat({'Uploaded data for '},dbName,'-',packageName, {'  aggr. '}, num2str(aggregatedTime), {'min.'}),'FontSize',titleFontSize);
+                ylabel('Uploaded data [kB]','FontSize',axisLabelFontSize);
+                set(gca, 'FontSize', axisLabelFontSize);
+                grid on;
+                ylim([minY_global, maxY_global]);
                 saveTightFigure(fig_upload, figureNameUpload);
             end
             
-            close all;
-            fig_download = plotBar(dataX_down_back, dataY_down_back, dataX_down_fore, dataY_down_fore, dataX_down_ibtw, dataY_down_ibtw, showParam, logYaxis, 'off');
-            title(strcat({'Downloaded data for '},dbName,'-',packageName, {'  aggr. '}, num2str(aggregatedTime), {'min.'}),'FontSize',titleFontSize);
-            ylabel('Downloaded data [kB]','FontSize',axisLabelFontSize);
-            set(gca, 'FontSize', axisLabelFontSize);
-            grid on;
-            ylim([minY_global, maxY_global]);
             if(numel([dataX_down_back(:)', dataX_down_fore(:)', dataX_down_ibtw(:)']) > 0)
+                close all;
+                fig_download = plotBar(dataX_down_back, dataY_down_back, dataX_down_fore, dataY_down_fore, dataX_down_ibtw, dataY_down_ibtw, showParam, logYaxis, 'off');
+                title(strcat({'Downloaded data for '},dbName,'-',packageName, {'  aggr. '}, num2str(aggregatedTime), {'min.'}),'FontSize',titleFontSize);
+                ylabel('Downloaded data [kB]','FontSize',axisLabelFontSize);
+                set(gca, 'FontSize', axisLabelFontSize);
+                grid on;
+                ylim([minY_global, maxY_global]);
                 saveTightFigure(fig_download, figureNameDownload);
             end
             
@@ -179,41 +184,43 @@ for nameIdx = 1 : numel(dbnames)
             %% PLOT POINTS
             titleFontSize = 28;
             axisLabelFontSize = 24;
-            %save figure and remove borders
-            figureNameUpload = strcat('figures/',dbName,'-',packageName,'-aggr',num2str(aggregatedTime), '-scatter-upload.pdf');
-            figureNameDownload = strcat('figures/',dbName,'-',packageName,'-aggr',num2str(aggregatedTime), '-scatter-download.pdf');
             
+            %save figure and remove borders
             close all;
-            fig_upload = plotDataPoint(dataX_back, dataY_back, dataX_fore, dataY_fore, dataX_ibtw, dataY_ibtw, showParam, logYaxis, 'off');
-            title(strcat({'Uploaded data for '},dbName,'-',packageName, {'  aggr. '}, num2str(aggregatedTime), {'min.'}),'FontSize',titleFontSize);
-            ylabel('Uploaded data [kB]','FontSize',axisLabelFontSize);
-            set(gca, 'FontSize', axisLabelFontSize);
-            grid on;
-            ylim([minY_global, maxY_global]);
+            
             if(numel([dataX_back', dataX_fore', dataX_ibtw']) > 0)
+                figureNameUpload = strcat('figures/',packageName,'-',dbName,'-aggr',num2str(aggregatedTime), '-scatter-upload.pdf');
+                figureNameDownload = strcat('figures/',packageName,'-',dbName,'-aggr',num2str(aggregatedTime), '-scatter-download.pdf');
+                
+                fig_upload = plotDataPoint(dataX_back, dataY_back, dataX_fore, dataY_fore, dataX_ibtw, dataY_ibtw, showParam, logYaxis, 'off');
+                title(strcat({'Uploaded data for '},dbName,'-',packageName, {'  aggr. '}, num2str(aggregatedTime), {'min.'}),'FontSize',titleFontSize);
+                ylabel('Uploaded data [kB]','FontSize',axisLabelFontSize);
+                set(gca, 'FontSize', axisLabelFontSize);
+                grid on;
+                ylim([minY_global, maxY_global]);
                 saveTightFigure(fig_upload, figureNameUpload);
             end
             
-            close all;
-            fig_download = plotDataPoint(dataX_down_back, dataY_down_back, dataX_down_fore, dataY_down_fore, dataX_down_ibtw, dataY_down_ibtw, showParam, logYaxis, 'off');
-            title(strcat({'Downloaded data for '},dbName,'-',packageName, {'  aggr. '}, aggregatedTime, {'min.'}),'FontSize',titleFontSize);
-            ylabel('Downloaded data [kB]','FontSize',axisLabelFontSize);
-            set(gca, 'FontSize', axisLabelFontSize);
-            grid on;
-            ylim([minY_global, maxY_global]);
             if(numel([dataX_down_back', dataX_down_fore', dataX_down_ibtw']) > 0)
+                close all;
+                fig_download = plotDataPoint(dataX_down_back, dataY_down_back, dataX_down_fore, dataY_down_fore, dataX_down_ibtw, dataY_down_ibtw, showParam, logYaxis, 'off');
+                title(strcat({'Downloaded data for '},dbName,'-',packageName, {'  aggr. '}, aggregatedTime, {'min.'}),'FontSize',titleFontSize);
+                ylabel('Downloaded data [kB]','FontSize',axisLabelFontSize);
+                set(gca, 'FontSize', axisLabelFontSize);
+                grid on;
+                ylim([minY_global, maxY_global]);
                 saveTightFigure(fig_download, figureNameDownload);
             end
-
+            
         end
         
         %save figure and remove borders
         
         %display some stats:
         display(strcat({'###### '}, dbName, '-', packageName));
-%         display(strcat({'Uploaded data on background: '}, num2str(sum(dataY_back))));
-%         display(strcat({'Uploaded data on foreground: '}, num2str(sum(dataY_fore))));
-%         display(strcat({'Uploaded data on iwbt: '}, num2str(sum(dataY_ibtw))));
+        %         display(strcat({'Uploaded data on background: '}, num2str(sum(dataY_back))));
+        %         display(strcat({'Uploaded data on foreground: '}, num2str(sum(dataY_fore))));
+        %         display(strcat({'Uploaded data on iwbt: '}, num2str(sum(dataY_ibtw))));
         
         display('next');
         %pause;
